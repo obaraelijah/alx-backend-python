0fe930cc40e6f7d09dcbf704e7e1a5b36dff3e81
elijah obara <elijahobara357@gmail.com>
2023-07-10 10:06:22 +0300

    basic add annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that has type annotation
+on its parameters and returns their sum as a float
+"""
+
+def add(a: float, b: float) -> float:
+    """Returns sum of a and b"""
+    return a + b
