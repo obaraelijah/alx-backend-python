d70bc9c5afc870d248f9b67430e94adeeba7306a
elijah obara <elijahobara357@gmail.com>
2023-07-10 10:20:29 +0300

    Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""This module contains a function that has type annotation
-on its parameters and return value
-"""
-
-
-def add(a: float, b: float) -> float:
-    """Returns sum of a and b"""
-    return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""This module implements a type annotated function
-for strings
-"""
+"""This module implements a type annotated function for strings"""
 
 
 def concat(str1: str, str2: str) -> str:
