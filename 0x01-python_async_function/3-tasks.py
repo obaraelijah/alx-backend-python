32b066bea4a8e0be60f97ac562324148998103bc
elijah obara <elijahobara357@gmail.com>
2023-07-10 14:26:54 +0300

    creating asyncion task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Asyncio Tasks'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''returns asynchronous task for wait_random'''
+    return asyncio.create_task(wait_random(max_delay))
