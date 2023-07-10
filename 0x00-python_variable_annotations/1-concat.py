a94c5c3cce1e60ce10b106f310b80e1aaad2883b
elijah obara <elijahobara357@gmail.com>
2023-07-10 10:15:49 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module implements a type annotated function
+for strings
+"""
+
+def concat(str1: str, str2: str) -> str:
+    "returns a concatnated string"
+    return str1 + str2
