177b0b265b560d82b1ff7e582352f37eb6a47dc9
elijah obara <elijahobara357@gmail.com>
2023-07-10 10:41:06 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module implements a function that converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string"""
+    return str(n)
