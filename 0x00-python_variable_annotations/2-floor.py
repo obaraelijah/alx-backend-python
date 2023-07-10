c576b898c0bbad160b6891ac9d05546f54ec4c0a
elijah obara <elijahobara357@gmail.com>
2023-07-10 10:28:58 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module has a function that returns the floor of its argument"""
+
+import math
+
+def floor(n: float) -> int:
+    """Returns floor of n"""
+    return math.floor(n)
