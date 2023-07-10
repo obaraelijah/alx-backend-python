1d4223463919de185d452447d053d34ec832fbfd
elijah obara <elijahobara357@gmail.com>
2023-07-10 14:32:41 +0300

    runtime calculation

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''basics of async'''
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''measures runtime of wait_n'''
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    return (end_time - start_time) / n
